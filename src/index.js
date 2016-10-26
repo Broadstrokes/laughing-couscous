1d05bfa4c917dc602ff35a65d1ebe8f491b76737
Dan Sajjad <dan.sajjad@gmail.com>
2016-10-25 18:09:30 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App';
 import './index.css';
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Home'
+import Dentists from './Dentists'
+import Services from './Services'
+import Patient from './Patient'
+import Contact from './Contact'
 
-ReactDOM.render(
-  <App />,
+render(
+  <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="/home" component={Home}/>
+      <Route path="/dentists" component={Dentists}/>
+      <Route path="/services" component={Services}/>
+      <Route path="/patient" component={Patient}/>
+      <Route path="/contact" component={Contact}/>
+   </Route>
+  </Router>,
   document.getElementById('root')
 );
