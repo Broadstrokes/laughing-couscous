fdda9f5bd98bc0addda280b5c82e9cc65a543dce
Dan Sajjad <dan.sajjad@gmail.com>
2016-10-25 18:11:06 -0700

    Add services component

---
+++ src/Services.js
@@ -0,0 +1,303 @@
+import React, { Component } from 'react';
+
+export default class Services extends Component {
+  render() {
+    return (
+      <div className="container-fluid container-outer">
+
+        <div className="page-header">
+          <h1>Services</h1>
+        </div>
+
+      <div className="container-fluid container-services">
+
+        <section>
+            <div className="row">
+              <div className="col-md-12">
+                <table className="services-menu">
+                  <tr>
+                    <th className="menu-item"><a href="#exams-xrays">Exams and Digital X-Rays</a></th>
+                    <th className="menu-item"><a href="#periodontal-therapy">Periodontal Therapy</a></th>
+                    <th className="menu-item"><a href="#composite-restorations">Composite Restorations</a></th>
+                    <th className="menu-item"><a href="#dental-implants">Dental Implants</a></th>
+                    <th className="menu-item"><a href="#crowns">Dental Crowns</a></th>
+                    <th className="menu-item"><a href="#dental-bridges">Dental Bridges</a></th>
+                    <th className="menu-item"><a href="#porcelain-veneers">Porcelain Veneers</a></th>
+                    <th className="menu-item"><a href="#endodontics">Endodontics (Root Canals)</a></th>
+                    <th className="menu-item"><a href="#complete-dentures">Complete Dentures</a></th>
+                    <th className="menu-item"><a href="#partial-dentures">Partial Dentures</a></th>
+                    <th className="menu-item"><a href="#flippers">Dental Flippers</a></th>
+                    <th className="menu-item"><a href="#extractions">Dental Extractions</a></th>
+                  </tr>
+                </table>
+              </div>
+            </div>
+        </section>
+
+        <section className="row services-section" id="exams-xrays">
+          <div className="col-md-6">
+            <h3>Exams and Digital X-Rays</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                As a new patient, our dentists will evaluate your oral system to comprehensively 
+              determine what treatment options are recommended. This involves evaluating each tooth, 
+              checking for any signs of gum disease, biting problems, and also an oral cancer screening 
+              exam. Our office also provides digital x-rays instead of the traditional film system. 
+              Advantages of this include an almost instantaneous image of the area captured displayed 
+              on a monitor. This allows chairside explanation with the patient to determine what’s going on.
+              Digital x-rays also provide a more detailed resolution than films, making earlier detection 
+              and treatment of problems a reality. Most importantly, digital x-rays decreases radiation 
+              exposure to you.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-right" />
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="periodontal-therapy">
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-left" />
+          </div>
+          <div className="col-md-6">
+          <h3>Periodontal Therapy</h3>
+          <hr className="hr-services"></hr>
+            <p>
+                The foundation and surrounding structures of our teeth is known as the periodontium.
+              Many factors can cause the periodontium to become diseased and inflamed. If left untreated, 
+              this could cause receding gums, tooth loss, and subsequent drifting of other teeth which 
+              can change the appearance of your smile. We provide individualized specific treatment options 
+              for each of our patients to help maintain a healthy periodontium.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="composite-restorations">
+          <div className="col-md-6">
+            <h3>Composite Restorations</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                Composite Restorations – These are also known as white fillings. At our office, we are 
+              proud to provide bonded composite resin restorations as opposed to the traditional silver 
+              amalgams. These can be used to repair decayed and chipped teeth to help you restore your 
+              smile and bite. In today's day and age, the colours now match very well with the vast 
+              majority of people's teeth, such that even you may not notice that there's a filling there 
+              after we finish!
+            </p>
+            <p>
+                <strong>Procedure:</strong> As with all fillings, any decay or compromised tooth structure must first be 
+              removed by a dentist to ensure that the filling material lies on sound tooth structure. 
+              Then, the tooth is dried and prepared for the bonding agent to do its job to ready the tooth 
+              for the filling material. A bright blue light is then used to activate the bonding agent 
+              before the filling is placed. Then, our dentists will shape the filling to its correct 
+              anatomical form and then polish it so that it's hard for people to discern that you've had 
+              dental work done!
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-right" />
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="dental-implants">
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-left" />
+          </div>
+          <div className="col-md-6">
+            <h3>Dental Implants</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                An excellent option for many patients to replace a missing or soon-to-be-missing tooth. 
+              These are made of a biocompatible titanium alloy in the shape of a little screw that is 
+              placed into the bone to replace the roots of a tooth. After a period of healing time, the 
+              implant is then exposed, and an impression is taken of the structures around it. 
+              A laboratory then fabricates an implant-supported crown based on the shape of your teeth 
+              to restore esthetics and function.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="crowns">
+          <div className="col-md-6">
+            <h3>Dental Crowns</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                When a tooth is compromised such that a filling may not be the best option for the 
+              longevity of the tooth, a crown is a full coverage restoration that’ll protect the 
+              tooth more completely. These can be made from a variety of materials, such as all-ceramic, 
+              porcelain-fused-to-metal, or a gold alloy. This procedure requires 2 or more appointments 
+              spread out over a week or two, as a dental laboratory technician is needed to fabricate 
+              a crown custom made for your teeth. In the meantime, we will provide you with a temporary 
+              crown to help you protect the remaining tooth structure and to provide a “test-run” for 
+              what the final shape and bite of the tooth feels like.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-right" />
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="dental-bridges">
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-left" />
+          </div>
+          <div className="col-md-6">
+            <h3>Dental Bridges</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                A fixed, as opposed to removable (such as dentures), option to replace a missing tooth 
+              or teeth. This consists of 3 or more fused crowns extending from one anchor tooth to 
+              the other, with artificial teeth attached in the middle. Similar to crowns, these will 
+              usually require 2 appointments to complete.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="porcelain-veneers">
+          <div className="col-md-6">
+            <h3>Porcelain Veneers</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                These are fixed prosthetics used primarily to enhance the appearance of one’s front teeth. 
+              Candidate teeth include those that are slightly chipped, show superficial crack lines, 
+              or are discolored internally for a variety of reasons. Made of an all-ceramic material, 
+              these are bonded to your teeth similar to white fillings to ensure longevity. Similar to 
+              crowns, these usually require 2 appointments. In the transition waiting time while a dental 
+              laboratory makes the porcelain veneer, a temporary veneer will be fabricated for you to 
+              protect the tooth and your smile.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-right" />
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="endodontics">
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-left" />
+          </div>
+          <div className="col-md-6">
+            <h3>Endodontics (Root Canals)</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                Trauma to a tooth, either through blunt force or dental decay, can irritate the nerve of a tooth. 
+              If this irritation leads to bacteria entering the nerve, or pulp, of a tooth, the pulp often can’t 
+              heal itself and becomes infected. This infected pulp can cause pain and swelling that does not go 
+              away by itself. If this is the situation, a root canal therapy is indicated to remove the infected 
+              pulp to get rid of the symptom. Contrary to popular belief, root canals help relieve one from pain, 
+              not cause additional pain. 
+            </p>
+            <p> 
+                <strong>Procedure:</strong> To keep you comfortable, we first anesthesize the area. Then, we need to access the 
+              pulp chamber through the tooth in order to clean and shape the canals of the pulp, which is where 
+              the bacteria lie. After that, sometimes it’s best to leave some medication in the canal space to help 
+              disinfect the tooth even more, and at the next appointment, to fill the now clean canals with a 
+              natural inert tree rubber called gutta percha. Then, we have to restore the tooth, usually with a 
+              full coverage restoration such as a crown, or in some cases, a direct bonded composite restoration. 
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="complete-dentures">
+          <div className="col-md-6">
+            <h3>Complete Dentures</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                When all teeth are missing on either the upper or lower jaw, a complete denture can be fabricated 
+              to restore esthetics, form, and function. For additional stability and retention, implants can be 
+              used to secure a denture more firmly in place. These are custom made for each individual of course!
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-right" />
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="partial-dentures">
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-left" />
+          </div>
+          <div className="col-md-6">
+            <h3>Partial Dentures</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                When a few teeth are missing in one jaw, a removable partial denture can be a good option to restore 
+              these teeth. They can either have a metal backbone or can be all acrylic, depending on the situation. 
+              These are helpful in not only providing good biting power, but to prevent the drifting of nearby teeth 
+              which can result in a change in the alignment of your teeth and smile.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="flippers">
+          <div className="col-md-6">
+            <h3>Flippers</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                A subsection of partial dentures. These are often made as a temporary solution for people who are 
+              missing a front tooth due to trauma. Usually all acrylic, these depend on grabbing onto nearby 
+              teeth to replace a missing front tooth for esthetic reasons.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-right" />
+          </div>
+        </section>
+
+        <hr></hr>
+
+        <section className="row services-section" id="extractions">
+          <div className="col-md-6">
+            <img src="http://placehold.it/600x300" className="img-left" />
+          </div>
+          <div className="col-md-6">
+            <h3>Extractions</h3>
+            <hr className="hr-services"></hr>
+            <p>
+                Sometimes, despite our best efforts, a tooth cannot or should not be kept in the mouth. 
+              Our team will determine the diagnosis and prognosis of a tooth and whether or not a specialist 
+              is needed to remove the tooth. If so, we will communicate with the specialist and forward all 
+              x-rays and instructions to them. If a specialist is not required, one of our team members can 
+              take out the tooth for you. Then, if recommended, treatment options will be presented to you 
+              to determine the options to replace the missing tooth.
+            </p>
+            <a href="#"><div className="button">Back to top</div></a>
+          </div>
+        </section>
+
+      </div>
+      </div>
+    );
+  }
+}
